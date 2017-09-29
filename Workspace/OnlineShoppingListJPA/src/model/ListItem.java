2179d683c834d3f33953a75345bf9eb1a34a3faf
cbuchwald229 <31973954+cbuchwald229@users.noreply.github.com>
2017-09-28 20:58:24 -0500

    Here's my Store Shopping project

---
+++ Workspace/OnlineShoppingListJPA/src/model/ListItem.java
@@ -0,0 +1,69 @@
+//final annoated ListItem POJO
+package model;
+
+//All these import statements came in by clicking the red X and using Import option from the javax.persistence.package
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "items")
+public class ListItem
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	@Column(name="STORE")
+	private String store;
+	@Column(name="ITEM")
+	private String item;
+	
+	public ListItem()
+	{
+	}
+	
+	public ListItem(String s, String i)
+	{
+		this.store = s;
+		this.item = i;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public String getStore()
+	{
+		return store;
+	}
+	
+	public void setStore(String store)
+	{
+		this.store = store;
+	}
+	
+	public String getItem()
+	{
+		return item;
+	}
+	
+	public void setItem(String item)
+	{
+		this.item = item;
+	}
+
+	@Override
+	public String toString() {
+		return "ListItem [id=" + id + ", store=" + store + ", item=" + item + "]";
+	}
+}
