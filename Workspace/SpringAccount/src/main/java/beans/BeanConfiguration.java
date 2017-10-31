0ceea402d396cb9ef3dab7d386b0d0170bf36c9f
cbuchwald229 <31973954+cbuchwald229@users.noreply.github.com>
2017-10-31 13:28:45 -0500

    I cannot get it to work. Think I followed most instructions and got rid of errors. My brain is fried. Can you help?

---
+++ Workspace/SpringAccount/src/main/java/beans/BeanConfiguration.java
@@ -0,0 +1,30 @@
+package beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration
+{
+	@Bean
+	public AccountDao accountDao()
+	{
+		AccountDaoInMemoryImpl bean = new AccountDaoInMemoryImpl();
+		return bean;
+	}
+	
+	@Bean
+	public AccountService accountService()
+	{
+		AccountServiceImpl bean = new AccountServiceImpl(accountDao());
+		//bean.setAccountDao(accountDao());
+		return bean;
+	}
+	
+	@Bean
+	public AccountDao accountDaoJdbc()
+	{
+		AccountDaoJdbcImpl bean = new AccountDaoJdbcImpl();
+		return bean;
+	}
+}
