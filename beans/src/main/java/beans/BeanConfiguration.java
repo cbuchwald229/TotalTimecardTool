a3e891ee717472363506cbd4fa6fe2f536ef9268
cbuchwald229 <31973954+cbuchwald229@users.noreply.github.com>
2017-10-31 13:31:15 -0500

    Howdy

---
+++ beans/src/main/java/beans/BeanConfiguration.java
@@ -0,0 +1,28 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+	@Bean
+	public AccountDao accountDao(){
+		AccountDaoInMemoryImpl bean = new AccountDaoInMemoryImpl();
+		return bean;
+	}
+	
+	@Bean(autowire=Autowire.BY_NAME)
+	public AccountService accountService(){
+		AccountServiceImpl bean = new AccountServiceImpl();
+		//AccountServiceImpl bean = new AccountServiceImpl(accountDao());
+		//bean.setAccountDao(accountDao());
+		return bean;
+	}
+
+	@Bean(autowire=Autowire.BY_NAME)
+	public AccountDao accountDaoJdbc() {
+		AccountDaoJdbcImpl bean = new AccountDaoJdbcImpl();
+		return bean;
+	}
+}
